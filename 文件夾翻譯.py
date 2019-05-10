677d80ca98550c386621f1e2075c0893479522ba
Rimo <RimoChan@stu.xmu.edu.cn>
2019-05-10 22:56:51 +0800

    文件名也翻譯

--- 文件夾翻譯.py
+++ 文件夾翻譯.py
@@ -13,8 +13,8 @@ def 文件夾翻譯(路徑):
         if i.endswith('.py'):
             with open(f'{路徑}/{i}',encoding='utf8') as f:
                 結果 = 翻譯.處理(f.read())
-            with open(f'{路徑}/_翻譯/{i}','w',encoding='utf8') as f:
+            with open(f'{路徑}/_翻譯/{翻譯.處理(i)}','w',encoding='utf8') as f:
                 f.write(結果)
             
 if __name__=='__main__':
-    文件夾翻譯('D:/Librian_release')
+    文件夾翻譯('.')
